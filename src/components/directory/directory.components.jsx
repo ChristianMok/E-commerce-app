acbf5e43a6acf1bea1215ac3bc67ab8ef904e333
Huahui Mo <33701207+ChristianMok@users.noreply.github.com>
2020-07-16 13:49:22 -0600

    implement withRouter()

--- src/components/directory/directory.components.jsx
+++ src/components/directory/directory.components.jsx
@@ -11,29 +11,56 @@ class Directory extends React.Component {
                 {
                     title: 'Computer case',
                     imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-                    id: 1
+                    id: 1,
+                    linkUrl:'Computer case'
                 },
                 {
                     title: 'Cooling',
                     imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-                    id: 2
+                    id: 2,
+                    linkUrl:'Cooling'
                 },
                 {
                     title: 'Motherboards',
                     imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-                    id: 3
+                    id: 3 ,
+                    linkUrl:'Motherboards'
+
                 },
                 {
                     title: 'Processors',
                     imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
                     size: 'large',
-                    id: 4
+                    id: 4,
+                    linkUrl:'Processors'
                 },
                 {
                     title: 'Video Cards',
                     imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
                     size: 'large',
-                    id: 5
+                    id: 5,
+                    linkUrl:'Video Cards'
+                },
+                {
+                    title: 'Power Supplies',
+                    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+                    size: 'large',
+                    id: 6,
+                    linkUrl:'Power Supplies'
+                },
+                {
+                    title: 'Memory',
+                    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+                    size: 'large',
+                    id: 7,
+                    linkUrl: 'Memory'
+                },
+                {
+                    title: 'Sound Cards',
+                    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+                    size: 'large',
+                    id: 8,
+                    linkUrl: 'Sound Cards'
                 },
             ]
         };
@@ -44,8 +71,8 @@ class Directory extends React.Component {
     render() {
         return (
             <div className='directory-menu'>
-                {this.state.sections.map(({ title, imageUrl, id, size }) => (
-                    <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+                {this.state.sections.map(({ id, ...othersSectionProps}) => (
+                    <MenuItem key={id} {...othersSectionProps} />
                 ))}
             </div>
         );
